ba0c62d7b9a20acdf2ef3f3efcd2c1de3195745e
Chaitanya <budagavichaitu@gmail.com>
2023-11-21 22:44:52 +0530

    add screens

---
+++ src/navigator/BottomTab.js
@@ -0,0 +1,13 @@
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//
+// const BottomTab = () => {
+//   const BottomTabNavigator = createBottomTabNavigator();
+//
+//   return (
+//     <BottomTabNavigator.Navigator>
+//       <BottomTabNavigator.Screen screen={'Home'} component={} />
+//     </BottomTabNavigator.Navigator>
+//   )
+// };
+//
+// export default BottomTab;
