0484b66a1c0ec8f3dc472cc82fb024c3e6a880be
Chaitanya <budagavichaitu@gmail.com>
2023-11-22 18:59:16 +0530

    add colors to styles/Colors

---
+++ src/styles/Colors.js
@@ -0,0 +1,22 @@
+const blackPalette = {
+  darkGunMetal: '#1B262E',
+  arsenic: '#354349',
+};
+
+const bluePalette = {
+  dakElectricBlue: '#606D76',
+  cadetBlue: '#A9B4BC',
+  chineseSilver: '#C5CDD2',
+  brightGray: '#E7ECF0',
+};
+
+const whitePalette = {
+  ghostWhite: '#F8F9FB',
+};
+
+const Colors = {
+  ...blackPalette,
+  ...bluePalette,
+  ...whitePalette,
+};
+export default Colors;
