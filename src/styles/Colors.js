0e244b02be185d83ea6a9d1e64a8aa01f1cb1cc1
Chaitanya <budagavichaitu@gmail.com>
2023-11-22 19:09:44 +0530

    add new colors to styles/Colors.js

--- src/styles/Colors.js
+++ src/styles/Colors.js
@@ -8,15 +8,23 @@ const bluePalette = {
   cadetBlue: '#A9B4BC',
   chineseSilver: '#C5CDD2',
   brightGray: '#E7ECF0',
+  cyanCobaltBlue: '#2A4BA0',
+  catalinaBlue: '#153075',
 };
 
 const whitePalette = {
   ghostWhite: '#F8F9FB',
 };
 
+const yellowPalette = {
+  brightYellow: '#F9B023',
+  sunGlow: '#FFC83A',
+};
+
 const Colors = {
   ...blackPalette,
   ...bluePalette,
   ...whitePalette,
+  ...yellowPalette,
 };
 export default Colors;
